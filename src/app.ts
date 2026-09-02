01dd6a2c1cbeb6bfb4f0232e775867ef7849c620
hxscyprodz <hxscyprodz@gmail.com>
2026-09-02 16:26:07 +0200

    feat: Add the register user functionality

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import ErrorHandlingMiddleware from "./middlewares/ErrorHandling";
 import RouteNotFoundMiddleware from "./middlewares/ResourceNotFound";
 import { config } from "./config/envConfig";
+import AuthRoutes from "./routes/AuthRoutes";
 import ProvincesRoutes from "./routes/ProvincesRoutes";
 import DistrictsRoutes from "./routes/DistrictsRoutes";
 import StationsRoutes from "./routes/StationsRoutes";
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(config.NODE_ENV === "production" ? "combined" : "dev"));
 
+app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/provinces", ProvincesRoutes);
 app.use("/api/v1/districts", DistrictsRoutes);
 app.use("/api/v1/stations", StationsRoutes);
