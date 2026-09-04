0bc11ef60fef6c24dc322c5c5ad2ba6a4bbd6522
hxscyprodz <hxscyprodz@gmail.com>
2026-09-05 01:39:18 +0200

    feat: Add the age calculator function

---
+++ src/utils/CalculateAge.ts
@@ -0,0 +1,18 @@
+const CalculateAge = (dateOfBirth: string | Date) => {
+  const today = new Date();
+  const birthDate = new Date(dateOfBirth);
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+};
+
+export default CalculateAge;
