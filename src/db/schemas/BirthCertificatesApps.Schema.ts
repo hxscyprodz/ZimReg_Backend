862de29a585e9d541e79c212ea9bd85ccae79e5e
hxscyprodz <hxscyprodz@gmail.com>
2026-09-06 00:41:30 +0200

    fix: Fix the BirthCertificatesApplicationSchema

--- src/db/schemas/BirthCertificatesApps.Schema.ts
+++ src/db/schemas/BirthCertificatesApps.Schema.ts
@@ -2,9 +2,14 @@ import { pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 import { BirthCertificates } from "./BirthCertificate.Schema";
 import { Hospitals } from "./Hospital.Schema";
 import { Applications } from "./Applications.Schema";
+import { Stations } from "./Station.Schema";
+import { sex } from "../Columns.Helper";
 
 export const BirthCertificateApplications = pgTable("birth_applications", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
+  station: uuid("station")
+    .notNull()
+    .references(() => Stations.id),
   trackingId: varchar("tracking_id", { length: 12 })
     .notNull()
     .references(() => Applications.trackingId, { onDelete: "cascade" })
@@ -12,8 +17,11 @@ export const BirthCertificateApplications = pgTable("birth_applications", {
   firstName: varchar("first_name", { length: 100 }).notNull(),
   middleNames: varchar("middle_names", { length: 200 }),
   surname: varchar("surname", { length: 100 }).notNull(),
+  sex: sex("sex").notNull(),
+  placeOfBirth: varchar("place_of_birth", { length: 255 }).notNull(),
+  villageOfOrigin: varchar("village_of_origin").notNull(),
   address: varchar("address", { length: 255 }).notNull(),
-  hospitalOfBirth: uuid("hospital_of_birth")
+  hospital: uuid("hospital")
     .notNull()
     .references(() => Hospitals.id),
   motherIdNumber: varchar("mother_id_number", { length: 14 })
