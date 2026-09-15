311906c9c00e0258a8705239dff3164ac93c1ae5
hxscyprodz <hxscyprodz@gmail.com>
2026-09-15 19:32:30 +0200

    refactor: Adds a unique index for the schedules station and date fields and add a not null constraint to the station field

--- src/db/schemas/Schedules.Schema.ts
+++ src/db/schemas/Schedules.Schema.ts
@@ -1,7 +1,19 @@
-import { pgTable, uuid, date, integer } from "drizzle-orm/pg-core";
+import { pgTable, uuid, date, integer, uniqueIndex } from "drizzle-orm/pg-core";
+import { Stations } from "./Station.Schema";
 
-export const Schedules = pgTable("schedules", {
-  id: uuid("id").primaryKey().defaultRandom().notNull(),
-  date: date("date").notNull().unique(),
-  capacity: integer("capacity").notNull().default(2),
-});
+export const Schedules = pgTable(
+  "schedules",
+  {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    date: date("date").notNull(),
+    station: uuid("station_id")
+      .references(() => Stations.id)
+      .notNull(),
+    capacity: integer("capacity").notNull().default(2),
+  },
+  (table) => {
+    return [
+      uniqueIndex("schedules_date_station_idx").on(table.date, table.station),
+    ];
+  },
+);
